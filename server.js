b4d67cdbd98bc3798f96f4b450f27955d9eed76d
gudals2040 <qd0420@gmail.com>
2025-10-01 15:10:31 +0900

    response2개 처리

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express"); // express 안에 있는 이미 구현되어
 const cors = require("cors");
 const dotenv = require("dotenv");
 const { createClient } = require("@supabase/supabase-js"); // 구조분해 할당
-
+const { GoogleGenAI } = require("@google/genai");
 dotenv.config(); // .env -> key
 // NODE -> process.env (환경변수) // cf. env file
 
@@ -23,7 +23,7 @@ const port = 3000; // cra. next -> express. / 5173.
 // CORS 해결을 미들웨어 적용
 app.use(cors()); // 모든 출처에 대한 허용 ( 보안적으로 바람적하지 X);
 
-app.use(express.json()) // req.body -> json.
+app.use(express.json()); // req.body -> json.
 // get, post...
 // app.방식(접속경로, 핸들러)
 // localhost:3000/
@@ -43,15 +43,118 @@ app.get("/plans", async (req, res) => {
   res.json(data);
 });
 
+app.get("/plans", async (req, res) => {
+  const { data, error } = await supabase.from("tour_plan").select("*");
+  if (error) {
+    return res.status(400).json({ error: error.message });
+  }
+  res.json(data);
+});
+
+// 1. SDK
+// 2. API Key <- .env
+// 3. 여러 단계를 프롬프팅 -> (값) => (프롬프트) => (결과값)
 app.post("/plans", async (req, res) => {
   const plan = req.body;
+  // ai를 통해
+  // npm install @google/genai
+  const ai = new GoogleGenAI({}); // GEMINI_API_KEY 알아서 인식해줌
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: `
+    [장소] ${plan.destination}
+    [목적] ${plan.purpose}
+    [인원수] ${plan.people_count}
+    [시작일] ${plan.start_date}
+    [종료일] ${plan.end_date}`,
+    config: {
+      // 형식을 구조화
+      responseMimeType: "application/json",
+      responseSchema: {
+        type: "object",
+        properties: {
+          prompt: {
+            type: "string",
+          },
+        },
+        required: ["prompt"],
+      },
+      systemInstruction: [
+        // { text: "제공받은 정보를 바탕으로 여행 계획을 짜되, 300자 이내로." },
+        {
+          text: `제공받은 정보를 바탕으로 최적의 여행 계획을 세우기 위한 프롬프트를 작성해줘. 응답은 JSON 형식으로 {"prompt": "프롬프트 내용"} 형식으로 작성해줘.`,
+        },
+      ],
+      // structured output
+    },
+  });
+  const { prompt } = JSON.parse(response.text); // bash에 출력
+  console.log("prompt", prompt);
+  const response2 = await ai.models.generateContent({
+    model: "gemini-2.5-flash-lite", // 모델을 상대적으로 약한 모델로...
+    contents: prompt,
+    config: {
+      systemInstruction: [
+        {
+          text: "프롬프트에 따라 작성하되, 300자 이내의 마크다운이 아닌 평문으로.",
+        },
+      ],
+    },
+  });
+  plan.ai_suggestion = response2.text; // 콘술에 출력
   const { error } = await supabase.from("tour_plan").insert(plan);
   if (error) {
     return res.status(400).json({ error: error.message });
   }
   res.status(201).json();
-}); // supabase table edtior db 연결
+});
 
+async function chaining(plan) {
+  const ai = new GoogleGenAI({}); // GEMINI_API_KEY 알아서 인식해줌
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: `
+    [장소] ${plan.destination}
+    [목적] ${plan.purpose}
+    [인원수] ${plan.people_count}
+    [시작일] ${plan.start_date}
+    [종료일] ${plan.end_date}`,
+    config: {
+      // 형식을 구조화
+      responseMimeType: "application/json",
+      responseSchema: {
+        type: "object",
+        properties: {
+          prompt: {
+            type: "string",
+          },
+        },
+        required: ["prompt"],
+      },
+      systemInstruction: [
+        // { text: "제공받은 정보를 바탕으로 여행 계획을 짜되, 300자 이내로." },
+        {
+          text: `제공받은 정보를 바탕으로 최적의 여행 계획을 세우기 위한 프롬프트를 작성해줘. 응답은 JSON 형식으로 {"prompt": "프롬프트 내용"} 형식으로 작성해줘.`,
+        },
+      ],
+      // structured output
+    },
+  });
+  const { prompt } = JSON.parse(response.text);
+  console.log("prompt", prompt);
+  const response2 = await ai.models.generateContent({
+    model: "gemini-2.5-flash-lite", // 모델을 상대적으로 약한 모델로...
+    contents: prompt,
+    config: {
+      systemInstruction: [
+        {
+          text: "프롬프트에 따라 작성하되, 300자 이내 plain text(no markdown or rich text)로.",
+        },
+      ],
+    },
+  });
+  return response2.text;
+}
 app.delete("/plans", async (req, res) => {
   const { planId } = req.body;
   const { error } = await supabase
